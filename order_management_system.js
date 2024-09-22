06e2ba114fb637fa12114dc1f1c369bfa646503e
Melissa Montesinos <melissa.d.montesinos@gmail.com>
2024-09-22 16:58:42 -0400

    Create a calculateOrderTotal function

--- order_management_system.js
+++ order_management_system.js
@@ -2,7 +2,7 @@
 // Task 1: Create an Inventory Array of Product Objects
 
 const inventory = [
-    {name: "Latte", price: 0.12, quantity: 120},
+    {name: "Latte", price: 4.50, quantity: 120},
     {name: "Cappuccino", price: 5, quantity: 90},
     {name: "Frappuccino", price: 2.79, quantity: 86},
     {name: "Cold Brew", price: 3.69, quantity: 75},
@@ -13,7 +13,7 @@ console.log(inventory);
 
 // Task 2: Create an array of Order Objects
 
-const orders = []; //Initialized Empty Array
+let orders = []; //Initialized Empty Array
    
 
 // Task 3: Create a Function to Place an Order
@@ -36,14 +36,33 @@ function placeOrder(customerName, orderedItems) {
                     customerName,
                     items: orderedItems,
                     status: 'Pending'
-            };
+            }; // If Sufficient Stock Dislay this message
 
         orders.push(recentOrder); // Push object at the last position
-        console.log(`Order placef for ${customerName}`)
+        console.log(`Order was placed for ${customerName}`);
     };
-    
-    placeOrder("Dayanna", [{name: "Latte", quantity: 2}, {name: "Cold Brew", quantity: 1}]); //Sample Order
-
 };
+
+
+// Task 4: Create a Function to Calculate Total for an Order
+
+function calculateOrderTotal(order) {
+    return order.items.reduce((sum, item) => {
+        const product = inventory.find(product => product.name === item.name);
+            return sum + (product.price * item.quantity)}, 0)};
+          
+placeOrder("Dayanna", [{name: "Latte", quantity: 2}, {name: "Cold Brew", quantity: 1}]); //Sample Order
+
+if (orders.length > 0) {
+    const ordertotal = calculateOrderTotal(orders[orders.length - 1]); // Defines the Function
+    console.log(`Dayanna's order total is $${ordertotal.toFixed(2)}`); // Message of order total
+};
+
+
+
+
+
+
+
 
 
