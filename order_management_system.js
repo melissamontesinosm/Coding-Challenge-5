ad447c288e36c2fe1702372637f81fae907fb29b
Melissa Montesinos <melissa.d.montesinos@gmail.com>
2024-09-22 15:45:44 -0400

    Create placeOrder function

--- order_management_system.js
+++ order_management_system.js
@@ -16,4 +16,34 @@ console.log(inventory);
 const orders = []; //Initialized Empty Array
    
 
+// Task 3: Create a Function to Place an Order
+
+function placeOrder(customerName, orderedItems) {
+    let Errorfound= false; // Identity which orders cannot be made due to insufficient stock 
+
+        orderedItems.forEach(item => {
+            let product = inventory.find(product => product.name === item.name);
+            if (!product || item.quantity > product.quantity ) {
+                console.log (`ERROR: ${item.name} has insufficient stock`);
+                Errorfound = true; // Insufffient Stock displays error
+            } else {
+                product.quantity -= item.quantity // Deduct quantity
+            }
+        });
+
+            if (!Errorfound) {
+                const recentOrder = {
+                    customerName,
+                    items: orderedItems,
+                    status: 'Pending'
+            };
+
+        orders.push(recentOrder); // Push object at the last position
+        console.log(`rder placef for ${customerName}`)
+    };
+    
+    placeOrder("Dayanna", [{name: "Latte", quantity: 2}, {name: "Cold Brew", quantity: 1}]); //Sample Order
+
+};
+
 
