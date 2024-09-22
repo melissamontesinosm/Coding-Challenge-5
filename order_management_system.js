e58d3e29a92f9ad09152fbb6e4224017cfb9bab0
Melissa Montesinos <melissa.d.montesinos@gmail.com>
2024-09-22 13:37:49 -0400

    Initialize inventory with product objects

---
+++ order_management_system.js
@@ -0,0 +1,13 @@
+
+// Task 1: Create an Inventory Array of Product Objects
+
+const inventory = [
+    {name: "Paper Cup", price: 0.12, quantity: 1200},
+    {name: "Ecuadorian Coffee Beans 12oz", price: 15, quantity: 60},
+    {name: "Almond Milk", price: 2.79, quantity: 86},
+    {name: "Brown Sugar 24oz", price: 3.69, quantity: 35},
+];
+
+console.log(inventory);
+
+
